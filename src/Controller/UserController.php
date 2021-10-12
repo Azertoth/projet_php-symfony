cbe64441766e812b934e283ed54cb78632d6aed9
jinyan YANG <jinyan.yang627@gmail.com>
2021-10-12 09:23:38 +0200

    switch to devjy

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,11 +9,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/login", name="login")
      */
     public function index(): Response
     {
-        return $this->render('user/index.html.twig', [
+        return $this->render('user/login.html.twig', [
             'controller_name' => 'UserController',
         ]);
     }
